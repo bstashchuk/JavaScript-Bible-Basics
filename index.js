16aaee46a77d78ecdf862c2fcca34dd494ce160f
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-10-23 14:33:05 +0300

    Initial

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+/**
+ * JS-BASICS/INTRO/CHALLENGE-1 - TASK
+ * 
+ * Create new .js file called "other.js"
+ * and in this file following line should
+ * be printed to the console:
+ * 
+ * "Greetings from other.js file!"
+ * 
+ * Add reference to this file to the "index.html"
+ */
 console.log("Hello from external JavaScript file");
