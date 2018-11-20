21379dd1835b59f193f63f1b7ae6158cf469d91d
Bogdan Stashchuk <bstashchuk@gmail.com>
2018-11-20 12:21:26 +0200

    Final

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-// /**
-//  * EXAMPLE 1
-//  * 
-//  * Try to "declare" function expression
-//  */
-// // It is not possible to use function expression standalone
-// function() { } //Uncaught SyntaxError: Unexpected token (
-
-// /**
-//  * EXAMPLE 2
-//  * 
-//  * Assign function expression to the variable
-//  */
-// const myFunction = function() { };
-
-// console.log(myFunction()); // undefined
-
-// console.log(myFunction); // ƒ () { }
-
-// /**
-//  * EXAMPLE 3
-//  * 
-//  * Callback function
-//  */
-// setTimeout(function() {
-//   console.log("Delayed message");
-// }, 3000);
-
-// /**
-//  * EXAMPLE 4
-//  * 
-//  * Callback function (example 2)
-//  */
-// let i = 1;
-// setInterval(function() {
-//   console.log("Message logged each 1 second " + i);
-//   i = i + 1;
-// }, 1000);
+/**
+ * CHALLENGE 01: TASK - Function Expressions
+ * 
+ * Use following built-in functions:
+ * setTimeout
+ * setInterval
+ * clearInterval
+ * 
+ * You should print to the console ONLY 5 messages with interval 2 seconds:
+ * "Here is message number 1"
+ * "Here is message number 2"
+ * "Here is message number 3"
+ * "Here is message number 4"
+ * "Here is message number 5"
+ */
