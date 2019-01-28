cac058b6adebc49a442701caf57d05351e08b1a3
Bogdan Stashchuk <bstashchuk@gmail.com>
2019-01-28 08:27:03 +0200

    Final

---
+++ index.js
@@ -0,0 +1,83 @@
+// /**
+//  * EXAMPLE 1
+//  * 
+//  * "if" statement
+//  */
+// let i = 10;
+
+// if (i > 0) {
+//   console.log(i);
+// }
+
+// if (i) {
+//   console.log(i);
+// }
+
+
+// /**
+//  * EXAMPLE 2
+//  * 
+//  * "if else" statement
+//  */
+// const myArray = [4, true, "abc", null, 5, 10];
+
+// if (myArray.length > 5) {
+//   console.log("Array is large");
+// } else {
+//   console.log("Array is small");
+// }
+
+// /**
+//  * EXAMPLE 3
+//  * 
+//  * Chaining "if" and "if else" statements
+//  */
+// let color = "green";
+
+// if (color === "green") {
+//   console.log("It is green color");
+// } else if (color === "yellow") {
+//   console.log("It is yellow color");
+// } else if (color === "red") {
+//   console.log("It is red color");
+// } else {
+//   console.log("Color is unknown");
+// }
+
+
+// /**
+//  * EXAMPLE 4
+//  * 
+//  * "switch" statement
+//  */
+// let color = "green";
+
+// switch (color) {
+//   case "green": 
+//     console.log("It is green color");
+//     break;
+//   case "yellow":
+//     console.log("It is yellow color");
+//     break;
+//   case "red":
+//     console.log("It is red color");
+//     break;
+//   default:
+//     console.log("Color is unknown");
+// }
+
+// /**
+//  * EXAMPLE 5
+//  * 
+//  * Ternary operator
+//  */
+// let i = 10;
+// let j;
+
+// j = i < 100 ? i : 0;
+
+// i > 10 && j
+//   ? console.log("Condition is truthy")
+//   : console.log("Condition is falsy");
+
+// console.log(j);
