a4f458ca25f1669edc6736ab553b2656deac93c1
Bogdan Stashchuk <bstashchuk@gmail.com>
2019-03-02 12:28:49 +0200

    start

--- index.js
+++ index.js
@@ -1,48 +1,17 @@
-// // PASS BY VALUE
-// // If variable that holds value of primitive value type is passed to the function as argument, it's value can't be changed inside of the function
-// function myFunction(parameterA) {
-//   parameterA = 20;
-//   return parameterA;
-// }
+// CHALLENGE
+// Create a function that will add custom method to the global object "window"
+function addCustomGlobalMethod(globalObject) {
+  globalObject.customMethod = function() {
+    console.log("Greeting from the custom method");
+  };
+}
 
-// const a = 10;
+addCustomGlobalMethod(this);
+console.log(this === window); // true
+console.log(typeof window); // "object"
 
-// console.log(myFunction(a)); // 20
+this.customMethod(); // "Greeting from the custom method"
 
-// console.log(a); // 10
+window.customMethod(); // "Greeting from the custom method"
 
-
-// // PASS BY REFERENCE
-// const myArray = [1, 2, 3];
-
-// console.log(myArray);
-
-// function myFunction(arr) {
-//   console.log(arr === myArray); // true
-//   arr.push(4);
-//   console.log(arr);
-//   return arr;
-// }
-
-// myFunction(myArray);
-
-// console.log(myArray);
-
-
-// // CHALLENGE
-// // Create a function that will add custom method to the global object "window"
-// function addCustomGlobalMethod(globalObject) {
-//   globalObject.customMethod = function() {
-//     console.log("Greeting from the custom method");
-//   };
-// }
-
-// addCustomGlobalMethod(this);
-// console.log(this === window); // true
-// console.log(typeof window); // "object"
-
-// this.customMethod(); // "Greeting from the custom method"
-
-// window.customMethod(); // "Greeting from the custom method"
-
-// customMethod(); // "Greeting from the custom method"
+customMethod(); // "Greeting from the custom method"
