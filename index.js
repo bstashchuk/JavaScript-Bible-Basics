e9b50c2302d5d7db2fe89cefbc552af1d3e33cac
Bogdan Stashchuk <bstashchuk@gmail.com>
2019-03-02 14:02:34 +0200

    final

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// CHALLENGE
-// Create a function "waitingFn" with single parameter "timeInMs" and inside of the function create "while" loop and this loop should be running during "timeInMs".
-// HINT: Date.now() - current time in ms since 1970
+// Events and Events queue
 function waitingFn(timeInMs) {
   const futureTime = Date.now() + timeInMs;
   while (futureTime > Date.now()) {
